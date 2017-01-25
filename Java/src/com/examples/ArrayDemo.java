102304efe5ffc6bf7e49b73462344a328d0b429a
Nick <nikosgn6@hotmail.com>
2017-01-25 22:15:42 +0200

    Reformat code and add Java Doc comment

--- Java/src/com/examples/ArrayDemo.java
+++ Java/src/com/examples/ArrayDemo.java
@@ -1,19 +1,25 @@
 package com.examples;
 
+/**
+ *  Demonstrates the declaration and the
+ *  printing of contents of an array of integers
+ *
+ *  @author Nikos K
+ *  @version 1.1
+ *
+ */
 public class ArrayDemo
 {
 
 	public static void main(String[] args)
 	{
 		int[] anArray = {100, 200, 300, 400, 500,
-				600, 700, 800, 900, 1000}; // Declares an array of ten integers
+				600, 700, 800, 900, 1000};
 		
 		// Print the array values.
 		for (int index = 0; index < anArray.length; index++)
 		{
-			System.out.println("Element at index "+(index + 1)+ ": "+
-			anArray[index]);
+			System.out.println("Element at index " + (index + 1 )+ ": " + anArray[index]);
 		}
-		
 	}
 }
