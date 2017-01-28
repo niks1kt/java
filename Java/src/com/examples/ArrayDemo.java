b5290dd25408ccb6d14c0eee131672bbb506fb99
Nick <nikosgn6@hotmail.com>
2017-01-28 17:13:50 +0200

    Reformat and change code
    - re-structure code
    - change array values

--- Java/src/com/examples/ArrayDemo.java
+++ Java/src/com/examples/ArrayDemo.java
@@ -2,24 +2,20 @@ package com.examples;
 
 /**
  *  Demonstrates the declaration and the
- *  printing of contents of an array of integers
+ *  display of contents of an integer array
  *
  *  @author Nikos K
- *  @version 1.1
+ *  @version 1.2
  *
  */
-public class ArrayDemo
-{
+public class ArrayDemo {
 
-	public static void main(String[] args)
-	{
-		int[] anArray = {100, 200, 300, 400, 500,
-				600, 700, 800, 900, 1000};
-		
-		// Print the array values.
-		for (int index = 0; index < anArray.length; index++)
-		{
-			System.out.println("Element at index " + (index + 1 )+ ": " + anArray[index]);
+	public static void main(String[] args) 	{
+		int[] intArray = {1, 2, 3, 4, 5};
+
+		// Prints at console the array values
+		for (int index = 0; index < intArray.length; index++) {
+			System.out.println("Element at index " + (index + 1) + ": " + intArray[index]);
 		}
 	}
 }
